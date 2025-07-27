877a6c2ae7c5667ce4221084c55270ef6c018761
root <root@DESKTOP-2QKKJ32>
2025-07-27 20:05:58 +0530

    Revert " small edit-1"
    This reverts commit ee0d034ecc02b44fd531e9a8f10c9da95ad893d4.

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const PORT = 3000;
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.end('<h1>Hello from Siddhi\'s DevOps Node.js App 🌟 🚀</h1>');
+  res.end('<h1>Hello from Siddhi\'s DevOps Node.js App 🚀</h1>');
 });
 
 server.listen(PORT, () => {
